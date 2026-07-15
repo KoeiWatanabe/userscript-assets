fdd7e326faccdb55d5e8bc4878c1b0498d35911b
KoeiWatanabe <koei917@gmail.com>
2026-07-15 09:52:02 +0900

    自作フォントに対応

--- tampermonkey/Excalifont Everywhere/script.js
+++ tampermonkey/Excalifont Everywhere/script.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Excalifont Everywhere
 // @namespace    https://tampermonkey.net/
-// @version      1.0.0
-// @description  Excalifont / Xiaolai をページ全体のフォントとして適用します
+// @version      1.2.0
+// @description  Excalidraw風-可変幅 をページ全体のフォントとして適用します
 // @author       You
 // @match        *://*/*
 // @exclude      *://www.bbc.com/*
 // @exclude      *://fonts.google.com/*
+// @exclude      *://www.canva.com/*
 // @grant        GM_addStyle
 // @run-at       document-start
 // @icon         https://excalidraw.framalab.org/apple-touch-icon.png
@@ -24,12 +25,38 @@
     }
 
     @font-face {
+      font-family: "BoostSetoFont";
+      src:
+        local("SetoFont"),
+        local("瀬戸フォント"),
+        local("Seto Font");
+    }
+
+    @font-face {
       font-family: "BoostXiaolai";
       src:
         local("Xiaolai SC"),
         local("Xiaolai"),
         local("小赖字体 SC"),
         local("小賴字體 SC");
+    }
+    @font-face {
+      font-family: "BoostExcalidrawVarWidth";
+      src:
+        local("Excalidraw風-可変幅"),
+        local("Excalidraw風-可変幅 Regular"),
+        local("Excalidraw-Style-Proportional"),
+        local("Excalidraw-Style-Proportional Regular"),
+        local("Excalidraw-Style-Proportional-Regular");
+    }
+    @font-face {
+      font-family: "BoostExcalidrawMono";
+      src:
+        local("Excalidraw風-等幅"),
+        local("Excalidraw風-等幅 Regular"),
+        local("Excalidraw-style-Mono"),
+        local("Excalidraw-style-Mono Regular"),
+        local("ExcalidrawStyleMono-Regular");
     }
 
     body,
@@ -57,7 +84,7 @@
       svg,
       svg *
     ) {
-      font-family: "BoostExcalifont", "BoostXiaolai", sans-serif !important;
+      font-family: "BoostExcalidrawVarWidth", sans-serif !important;
     }
   `);
 })();
